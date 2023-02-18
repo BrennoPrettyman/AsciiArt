cb27d9cbeeadfd4d883155c94fdbb8e7a22c394d
Brenno Prettyman <brennoprettyman@gmail.com>
2023-02-18 13:59:51 -0300

    Exercício feito

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AsciiArt;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("||   ||");
+        Console.WriteLine(@" \\()//");
+        Console.WriteLine(@"//(__)\\");
+        Console.WriteLine("||    ||");      
+        }
+}
